170bc810cd8ba10b1c9ce032f205039ea7833632
TDMeyers <meyers.dylan@outlook.com>
2023-06-13 14:18:54 -0600

    Task 2.0 - 2.3

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ mainEl.style.backgroundColor = "var(--main-bg)";
 mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
 
 mainEl.classList.add('flex-ctr');
+
+let topMenuEl = document.querySelector('#top-menu');
+
+topMenuEl.style.height = '100%';
+
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+
+topMenuEl.classList.add('flex-around');
+
+
+
